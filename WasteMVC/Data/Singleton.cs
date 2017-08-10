266ddb4857e03b7c7a24e5de99e62b3c005f7c76
CARLOS RATIA VILORIA <CRATIA@GMAIL.COM>
2017-08-09 20:44:40 -0400

    [A] Class Singleton

---
+++ WasteMVC/Data/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WasteMVC.Data
+{
+    public class Singleton
+    {
+        /// <summary>
+        /// Patrón Singleton
+        /// </summary>
+        protected static Singleton instance = null;
+        protected static readonly object padlock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public Singleton() { }
+    }
+}
